d181f441fed847773bfe3dfb93f22a19699a9cce
zz <family12345@126.com>
2017-06-12 03:04:29 -0700

    决策树写好了

---
+++ decisionTree/decisionTree.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 00:27:00 2017
+
+@author: zz
+"""
+import pandas as pd
+import numpy as np
+import math
+
+class Node:
+    def __init__(self, label):
+        self.label = label
+        self.attribute = None
+        self.children = None
+        self.x = None
+        self.y = None
+    def setAttribute(self, attribute):
+        self.attribute = attribute
+    def setChildren(self, children):
+        self.children = children
+    def p(self):
+        print 'label:', self.label, ', attribute:', self.attribute
+
+def entropy(obj):
+    # obj: pandas.Series
+    N = float(obj.count())
+    C = obj.value_counts().values
+    P = C / N
+    
+    H = 0.0
+    for p in P:
+        H += -p*math.log(p,2)
+    return H
+
+def conditionEntropy(obj, attribute, clazz):
+    """
+    obj: Pandas.DataFrame
+    attribute: string
+    clazz: string
+    """
+    CC = obj[attribute].value_counts()
+    index = CC.index
+    counts = CC.values
+    N = float(obj.index.size)
+ 
+    H = 0.0 # empirical entropy
+    HA = 0.0 # 训练数据集D关于特征A的值的熵
+    for i in range(index.size):
+        Hi = entropy(obj[obj[attribute] == index[i]][clazz])
+        p = counts[i] / N
+        H += p * Hi
+        HA += -p * math.log(p, 2)
+    return [H, HA]
+
+def decisionTree(obj, attributes, clazz, threshold, method='id3'):
+    """
+    obj: pandas.DataFrame
+    attributes: pandas.Series
+    clazz: string
+    threshold: float
+    """
+    clazz_value_counts = obj[clazz].value_counts()
+    label = clazz_value_counts.index[0]
+    node = Node(label)
+    
+    if clazz_value_counts.size == 1 or attributes.size == 0:
+        return node
+    
+    condition_entropy = np.zeros(attributes.size)
+    h = entropy(obj[clazz])
+    for i in range(attributes.size):
+        [condition_entropy[i], ha]= conditionEntropy(obj, attributes[i], clazz)
+        condition_entropy[i] = h - condition_entropy[i]
+        if method == 'c4.5':
+            condition_entropy[i] = condition_entropy[i] / ha
+    
+    index = condition_entropy.argmax()
+    attr = attributes[index]
+    node.setAttribute(attr)
+    
+    if condition_entropy.max < threshold:
+        return node
+    
+    attr_value_counts = obj[attr].value_counts()
+    attrs = attributes.drop(attr)
+    children = dict()
+    for i in range(attr_value_counts.size):
+        children[attr_value_counts.index[i]] = decisionTree(obj[obj[attr]==attr_value_counts.index[i]], attrs, clazz, threshold, method)
+    
+    node.setChildren(children)
+    return node
+
+df = pd.read_csv('贷款申请样本.txt', encoding='utf-8', index_col='ID')
+tree = decisionTree(df, df.columns.drop('clazz'), 'clazz', 0)
+
+
+"""
+return tree depth
+"""
+def getTreeDepth(tree):
+    if tree.children == None:
+        return 1
+    
+    ds = [None]*len(tree.children)
+    i = 0
+    for node in tree.children.values():
+        ds[i] = getTreeDepth(node)
+        i = i + 1
+    
+    return max(ds)+1
+
+"""
+return the tree's leaves's number
+"""
+def getLeavesNum(tree):
+    if tree.children == None:
+        return 1
+    
+    num = 0
+    for node in tree.children.values():
+        num += getLeavesNum(node)
+
+    return num
--- decisionTree/treePlotter.py
+++ decisionTree/treePlotter.py
@@ -7,4 +7,67 @@ Created on Sat Jun  3 00:20:45 2017
 """
 
 import matplotlib.pyplot as plt
+import pandas as pd
+import decisionTree as dt
+
+def xy(tree, y):
+    global n
+    tree.y = y
+    
+    if tree.x != None:
+        return tree.x
+    
+    if tree.children == None:
+        x = n*x0
+        n += 1
+    else:
+        xSum = 0
+        k = len(tree.children)
+        for node in tree.children.values():
+            xSum += xy(node, y-y0)
+        x = float(xSum) / k
+
+    tree.x = x
+    return x
+
+xx = 0.05
+yy = 0.08
+
+def pp(tree, ax):
+    if tree == None:
+        return
+    
+    if tree.children != None:
+        for key in tree.children.keys():
+            ax.annotate(tree.attribute,
+                        xy=(tree.children[key].x+xx, tree.children[key].y+yy),
+                        xytext=(tree.x, tree.y),
+                        xycoords='figure fraction',
+                        textcoords='figure fraction',
+                        arrowprops=dict(arrowstyle='->')
+                           )
+            ax.text((tree.children[key].x+tree.x)/2, (tree.children[key].y+tree.y)/2, key, transform=ax.transAxes)
+            pp(tree.children[key], ax)
+    else:
+        ax.text(tree.x, tree.y, tree.label, transform=ax.transAxes)
+        
+        
+df = pd.read_csv('贷款申请样本.txt', encoding='utf-8', index_col='ID')
+tree = dt.decisionTree(df, df.columns.drop('clazz'), 'clazz', 0, method='c4.5')
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+h = dt.getTreeDepth(tree)
+w = dt.getLeavesNum(tree)
+
+y0 = float(1)/(h+1)
+x0 = float(1)/(w+1)
+
+n = 1
+
+xy(tree, 1-y0)
+pp(tree, ax)
+
+plt.show()
 
